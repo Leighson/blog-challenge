39d4ecf92bd4f8a86d6fe45c82061b3be764666f
Lee Santos <lee.m.santos@outlook.com>
2023-06-07 13:36:51 -0400

    Create function to connect to db client.

--- app.js
+++ app.js
@@ -3,14 +3,47 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const PORT = 3000;
+
+require('dotenv').config();
+const SERVER_USERID = process.env.ORIGIN_USERID;
+const SERVER_KEY = process.env.ORIGIN_KEY;
+const DATABASE_NAME = "blogEntries";
 
 let posts = [];
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${SERVER_USERID}:${SERVER_KEY}@origin.howe4yr.mongodb.net/${DATABASE_NAME}`;
+const client = new MongoClient(
+  uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  }
+);
+
+async function mongoConnect() {
+  try {
+    await client.connect();
+    console.log("Connected to mongo client.");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const homePost = new Post({
+  title: "Hello",
+  content: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+});
+
+console.log(homePost.title);
+console.log(homePost.content);
 
 // set static folder to public, defines access path to styling elements
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -47,16 +80,28 @@ app.get('/compose', (req, res) => {
 
 app.post('/compose', (req, res) => {
 
+  const composeTitle = req.body.postTitle;
+  const composeBody = req.body.postBody;
+
+  const PostsSchema = {
+    title: String,
+    content: String
+  };
+  
+  const Post = mongoose.model("Post", PostsSchema);
+
   const post = {
     title: req.body.postTitle,
     titleRequest: _.kebabCase(req.body.postTitle),
     body: req.body.postBody
   };
+
   posts.push(post);
   res.redirect('/');
+
 });
 
-//POSTS
+// POSTS
 app.get('/posts/:postTitle', (req, res) => {
   posts.forEach( (post) => {
     if (_.lowerCase(req.params.postTitle) === _.lowerCase(post.title)) {
@@ -69,6 +114,11 @@ app.get('/posts/:postTitle', (req, res) => {
 });
 
 
+/* DEPLOY APP on RAILWAY PORTS or USE 3000 if LOCAL */
+let PORT = process.env.PORT;
+if (PORT == null || PORT === "") {
+  PORT = 3000;
+}
 
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}`);
